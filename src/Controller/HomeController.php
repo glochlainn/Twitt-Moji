c285149b9158d68e0a0f85329d0a71cabf746f84
Wone <lochlainn.gadault@gmail.com>
2021-07-21 14:55:07 +0200

    feed of tweets

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TweetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,14 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(TweetRepository $tweetRepository): Response
     {
+        $tweets = $tweetRepository->findBy([], [
+            'tweeted_at' => 'DESC'
+        ]);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'tweets' => $tweets,
         ]);
     }
 }
