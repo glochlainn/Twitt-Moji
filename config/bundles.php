4ebf1593a71f2d421b250ec45bcb537d00cf83d6
Wone <lochlainn.gadault@gmail.com>
2021-07-22 10:20:11 +0200

    added modal list of follows and followers on profile

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Eschmar\TimeAgoBundle\EschmarTimeAgoBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
